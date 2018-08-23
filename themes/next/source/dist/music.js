7e30b9685017502a84e8d46ed1a964f9877029fe
athlonreg <15563836030@163.com>
2018-08-23 11:04:28 +0800

    常规更新

--- themes/next/source/dist/music.js
+++ themes/next/source/dist/music.js
@@ -7,19 +7,19 @@ const ap = new APlayer({
         name: "一首关于理想的歌",
         artist: '小闯',
         url: 'http://music.163.com/song/media/outer/url?id=406752046.mp3',
-        cover: 'https://github.com/athlonreg/BlogImages/blob/master/Images/blogcover/yishouguanyulixiangdege.jpg',
+        cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/yishouguanyulixiangdege.jpg',
       },
       {
         name: '成都',
         artist: '赵雷',
         url: 'http://music.163.com/song/media/outer/url?id=436514312.mp3',
-        cover: 'https://github.com/athlonreg/BlogImages/blob/master/Images/blogcover/chengdu.jpg',
+        cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/chengdu.jpg',
       },
       {
         name: '化身孤岛的鲸',
         artist: '李逸朗',
         url: 'http://music.163.com/song/media/outer/url?id=30987882.mp3',
-        cover: 'https://github.com/athlonreg/BlogImages/blob/master/Images/blogcover/huashengudaodejing.jpg',
+        cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/huashengudaodejing.jpg',
       }
     ]
 });
