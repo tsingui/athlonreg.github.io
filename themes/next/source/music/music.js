3b3276e42d724dc918de7dced3d1abd599c80cea
canvas <15563836030@163.com>
2018-10-28 21:30:28 +0800

    常规更新

--- themes/next/source/music/music.js
+++ themes/next/source/music/music.js
@@ -4,13 +4,13 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "怀念青春",
+        name: '怀念青春',
         artist: '刘刚',
         url: 'http://music.163.com/song/media/outer/url?id=29800567.mp3',
         cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/huainianqingchun.jpg',
       },
       {
-        name: "一首关于理想的歌",
+        name: '一首关于理想的歌',
         artist: '小闯',
         url: 'http://music.163.com/song/media/outer/url?id=406752046.mp3',
         cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/yishouguanyulixiangdege.jpg',
@@ -26,6 +26,12 @@ const ap = new APlayer({
         artist: '李逸朗',
         url: 'http://music.163.com/song/media/outer/url?id=30987882.mp3',
         cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/huashengudaodejing.jpg',
+      },
+      {
+        name: '沙漠骆驼',
+        artist: '展展与罗罗',
+        url: 'http://music.163.com/song/media/outer/url?id=486814412.mp3',
+        cover: 'https://raw.githubusercontent.com/athlonreg/BlogImages/master/Images/blogcover/shamoluotuo.jpg',
       }
     ]
 });
