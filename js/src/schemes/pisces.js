b1933725f307114803e5a49e5f135f3feb7532d9
athlonreg <15563836030@163.com>
2019-03-30 21:12:37 +0800

    Site updated: 2019-03-30 21:12:34

--- js/src/schemes/pisces.js
+++ js/src/schemes/pisces.js
@@ -1,3 +1,57 @@
-// build time:Tue Mar 12 2019 00:50:31 GMT+0800 (China Standard Time)
-$(document).ready(function(){var t=$(".sidebar-inner");e();i();function e(){var e=n(),i=f(),o=$("#sidebar").height()+NexT.utils.getSidebarb2tHeight(),a=$("#content").height();if(e+o<a){t.affix({offset:{top:e-CONFIG.sidebar.offset,bottom:i}})}r(e).css({"margin-left":"initial"})}function i(){var t=window.matchMedia("(min-width: 991px)");t.addListener(function(t){if(t.matches){o()}})}function n(){return $(".header-inner").height()+CONFIG.sidebar.offset}function f(){var t=$(".footer-inner"),e=t.outerHeight(true)-t.outerHeight(),i=t.outerHeight(true)+e;return i}function r(t){return $("#sidebar").css({"margin-top":t})}function o(){$(window).off(".affix");t.removeData("bs.affix").removeClass("affix affix-top affix-bottom");e()}});
-//rebuild by neat 
+/* global NexT, CONFIG */
+
+$(document).ready(function() {
+
+  var sidebarInner = $('.sidebar-inner');
+  var sidebarOffset = CONFIG.sidebar.offset || 12;
+
+  function getHeaderOffset() {
+    return $('.header-inner').height() + sidebarOffset;
+  }
+
+  function getFooterOffset() {
+    var footer = $('#footer');
+    var footerInner = $('.footer-inner');
+    var footerMargin = footer.outerHeight() - footerInner.outerHeight();
+    var footerOffset = footer.outerHeight() + footerMargin;
+    return footerOffset;
+  }
+
+  function initAffix() {
+    var headerOffset = getHeaderOffset();
+    var footerOffset = getFooterOffset();
+    var sidebarHeight = $('#sidebar').height() + NexT.utils.getSidebarb2tHeight();
+    var contentHeight = $('#content').height();
+
+    // Not affix if sidebar taller than content (to prevent bottom jumping).
+    if (headerOffset + sidebarHeight < contentHeight) {
+      sidebarInner.affix({
+        offset: {
+          top   : headerOffset - sidebarOffset,
+          bottom: footerOffset
+        }
+      });
+      sidebarInner.affix('checkPosition');
+    }
+
+    $('#sidebar').css({ 'margin-top': headerOffset, 'margin-left': 'auto' });
+  }
+
+  function recalculateAffixPosition() {
+    $(window).off('.affix');
+    sidebarInner.removeData('bs.affix').removeClass('affix affix-top affix-bottom');
+    initAffix();
+  }
+
+  function resizeListener() {
+    var mql = window.matchMedia('(min-width: 992px)');
+    mql.addListener(function(e) {
+      if (e.matches) {
+        recalculateAffixPosition();
+      }
+    });
+  }
+
+  initAffix();
+  resizeListener();
+});
