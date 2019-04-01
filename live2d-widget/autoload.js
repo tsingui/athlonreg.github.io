b2c6cdc65f1f38ff623a509d70d08163447f096c
athlonreg <15563836030@163.com>
2019-04-01 21:54:15 +0800

    Site updated: 2019-04-01 21:54:13

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,3 +1,3 @@
-// build time:Mon Apr 01 2019 21:48:59 GMT+0800 (中国标准时间)
+// build time:Mon Apr 01 2019 21:53:11 GMT+0800 (中国标准时间)
 const live2d_path="/live2d-widget/";$("<link>").attr({href:live2d_path+"waifu.css",rel:"stylesheet",type:"text/css"}).appendTo("head");$.ajax({url:live2d_path+"live2d.min.js",dataType:"script",cache:true,async:false});$.ajax({url:live2d_path+"waifu-tips.js",dataType:"script",cache:true,async:false});$(window).on("load",function(){initWidget(live2d_path+"waifu-tips.json","https://live2d.fghrsh.net/api")});
 //rebuild by neat 
